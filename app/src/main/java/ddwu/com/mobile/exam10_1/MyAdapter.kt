ce612ee5faf451b66266bb128ec789ce834781ab
aaasa8 <113534445+aaasa8@users.noreply.github.com>
2023-05-08 21:14:01 +0900

    Update MyAdapter.kt

--- app/src/main/java/ddwu/com/mobile/exam10_1/MyAdapter.kt
+++ app/src/main/java/ddwu/com/mobile/exam10_1/MyAdapter.kt
@@ -5,15 +5,21 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
 class MyAdapter(val context: Context, val layout: Int, val list: ArrayList<String>): RecyclerView.Adapter<MyAdapter.MyViewHolder>(){
+		// 각 항목의 뷰를 보관하는 Holder
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyAdapter.MyViewHolder {
-        TODO("Not yet implemented")
+        val view = LayoutInflater.from(context).inflate(layout, parent, false)
+				return MyViewHolder(view)
     }
-
+		// Holder에 보관중인 뷰에 원본 데이터 연결
     override fun onBindViewHolder(holder: MyAdapter.MyViewHolder, position: Int) {
-        
+        holder.tvText.text = list[position]
     }
-
+		// recyclerView에 표시할 전체 뷰의 개수 == 원본 데이터의 개수
     override fun getItemCount(): Int {
         return list.size
     }
+		// 항목의 뷰를 생성한 후 멤버변수로 보관하는 ViewHolder
+		class MyViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+			val tvText : TextView = view.findViewById(R.id.tvText)
+		}
 }
